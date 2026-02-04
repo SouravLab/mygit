243d447167905e84d2bb8862623152b2b2bdba47
SouravLab <souravmishra612@gmail.com>
2026-02-04 11:08:54 +0530

    customer class added...

---
+++ src/allImportantProgram/Customer.java
@@ -0,0 +1,10 @@
+package allImportantProgram;
+
+public class Customer {
+
+	public static void main(String[] args) {
+	System.out.println("this is customer class");
+	System.out.println("this is customer class2");
+	}
+
+}
